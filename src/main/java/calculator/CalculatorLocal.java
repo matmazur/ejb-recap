f6ab1da6bae59f681cefc3b5f52349bff7fce59c
Bingo Bobo <matmazur90@gmail.com>
2018-10-02 16:07:20 +0200

    added singleton annotated bean to count visits with AtomicInteger for better sync

--- src/main/java/calculator/CalculatorLocal.java
+++
@@ -1,8 +0,0 @@
-package calculator;
-
-import javax.ejb.Local;
-
-@Local
-public interface CalculatorLocal {
-    public double add(double a, double b);
-}
---
+++ src/main/java/dao/SingletonCounter.java
@@ -0,0 +1,29 @@
+package dao;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Singleton
+@Startup
+public class SingletonCounter {
+
+
+
+    private AtomicInteger visits;
+
+    @PostConstruct
+    private void init(){
+        visits = new AtomicInteger();
+    }
+
+    public int getVisits(){
+
+
+        return visits.incrementAndGet();
+    }
+
+
+
+}
