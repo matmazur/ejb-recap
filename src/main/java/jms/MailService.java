79f944e1f18473fae12c8d6f68f6baa4c1d89217
Bingo Bobo <matmazur90@gmail.com>
2018-10-02 21:16:28 +0200

    created class MailService - needs to be finished

---
+++ src/main/java/jms/MailService.java
@@ -0,0 +1,22 @@
+package jms;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
+
+public class MailService {
+
+
+    @Inject
+    private JMSContext context;
+
+    @Resource (mappedName = "jms/queue/Email")
+    private Queue queue;
+
+
+    public void sendEmail(MessageWrapper message){
+
+    }
+
+}
