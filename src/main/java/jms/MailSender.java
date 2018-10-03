5bb249e7da96581e35077f8d8dbcdfd7826138a1
Bingo Bobo <matmazur90@gmail.com>
2018-10-03 15:08:17 +0200

    added MailSender that listens destination that we picked for messages in queue

---
+++ src/main/java/jms/MailSender.java
@@ -0,0 +1,36 @@
+package jms;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+
+
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(
+                propertyName = "destination",
+                propertyValue = "java:/jms/queue/Email"),
+})
+public class MailSender implements MessageListener {
+
+
+
+
+
+    @Override
+    public void onMessage(Message message) {
+
+
+        MessageWrapper wrapper = null;
+        try {
+            wrapper = message.getBody(MessageWrapper.class);
+
+            System.out.println("Msg goes to " + wrapper.getToWhom());
+            System.out.println("Msg : " + wrapper.getMessage());
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
